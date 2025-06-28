066e3acff56265e65d7b9001e0d9c7c362972073
Waleed khan <151666902+waleedkhan69@users.noreply.github.com>
2025-06-28 15:33:58 +0500

    crud operation

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ import { dbConnection } from "./dbConnection.js";
 import { Enquery } from "./models/enquery.model.js";
 import dotenv from "dotenv"
 import cors from "cors"
-
-
 import { EnqueriRoute } from "./Controllers/Routes/Enquery.route.js";
 dotenv.config();
 const app = express()
@@ -13,11 +11,6 @@ const port = process.env.PORT || 5000
 app.use(express.json())
 app.use(cors())
 // app.use("/api", EnqueriRoute)
-
-
-
-
-
 app.use("/api/enquery", EnqueriRoute)
 
 app.listen(port, () => {
