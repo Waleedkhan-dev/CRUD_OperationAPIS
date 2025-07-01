f2b18550434c3cf503c7e4298fdc4d26343fd5a7
Waleed khan <151666902+waleedkhan69@users.noreply.github.com>
2025-07-01 23:50:08 +0500

    webpack

--- dbConnection.js
+++ dbConnection.js
@@ -1,10 +1,10 @@
-// import mongoose from "mongoose";
-// const dbConnection = async () => {
-//  try {
-//   const connect = await mongoose.connect(process.env.MONGODB_URL)
-//   console.log("Mongo DB connected Successfully");
-//  } catch (error) {
-//   console.log("mongo db failed");
-//  }
-// }
-// export { dbConnection }
+import mongoose from "mongoose";
+const dbConnection = async () => {
+ try {
+  const connect = await mongoose.connect(process.env.MONGODB_URL)
+  console.log("Mongo DB connected Successfully");
+ } catch (error) {
+  console.log("mongo db failed");
+ }
+}
+export { dbConnection }
